1904af6fb477d125a23b2d1e5c0453aa231cbbd1
Aman Shukla <142072904+amanBNF@users.noreply.github.com>
2026-04-18 15:03:46 +0530

    server

--- Backend/server.js
+++ Backend/server.js
@@ -1,13 +1,29 @@
-require("dotenv").config()
-const app = require("./src/app")
-const connectToDB = require("./src/config/database")
+// require("dotenv").config()
+// const app = require("./src/app")
+// const connectToDB = require("./src/config/database")
 
-connectToDB()
+// connectToDB()
 
+// app.get("/", (req, res) => {
+//     res.send("Hello World")
+// })
+
+// app.listen(3000, () => {
+//     console.log("Server is running on port 3000")
+// })
+
+require("dotenv").config();
+
+const app = require("./src/app");
+const connectToDB = require("./src/config/database");
+
+// connect DB
+connectToDB();
+
+// test route
 app.get("/", (req, res) => {
-    res.send("Hello World")
-})
+  res.send("Hello World");
+});
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000")
-})
+// ✅ export instead of listen
+module.exports = app;
