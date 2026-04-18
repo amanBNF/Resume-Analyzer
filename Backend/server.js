fe8826dfc5df959aca61c2953152ec39ae096b74
Aman Shukla <142072904+amanBNF@users.noreply.github.com>
2026-04-18 14:56:12 +0530

    get

--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,9 @@ const connectToDB = require("./src/config/database")
 
 connectToDB()
 
+app.get("/", (req, res) => {
+    res.send("Hello World")
+})
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000")
